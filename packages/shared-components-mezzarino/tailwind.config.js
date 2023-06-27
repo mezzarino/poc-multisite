f92d9e13a60e9b842c572549c27429b8aae17291
James Merriman <mezzarino@outlook.com>
2023-06-27 19:59:25 +0100

    Ensure component doesnt need shared configs

--- packages/shared-components-mezzarino/tailwind.config.js
+++ packages/shared-components-mezzarino/tailwind.config.js
@@ -1,7 +1,21 @@
-const sharedConfig = require("tailwind-config/tailwind.config.js");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   // prefix ui lib classes to avoid conflicting with the app
   prefix: "ui-",
-  presets: [sharedConfig],
+  content: [
+    // app content
+    `src/**/*.{js,ts,jsx,tsx}`,
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandblue: colors.blue[500],
+        brandred: colors.red[500],
+      },
+    },
+  },
+  plugins: [],
 };
