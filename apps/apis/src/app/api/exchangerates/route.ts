ab1b9a79a88dc26415dd5cd9cd458f478db89a30
James Merriman <mezzarino@outlook.com>
2023-07-04 21:03:00 +0100

    Fix API build

--- apps/apis/src/app/api/exchangerates/route.ts
+++ apps/apis/src/app/api/exchangerates/route.ts
@@ -1,26 +1,24 @@
-import { NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export async function GET(res: NextApiResponse) {
-  const data = [
-    {
-      id: 1,
-      country: "European Union",
-      currency: "Euro (EUR)",
-      rate: 1.1348,
-    },
-    {
-      id: 2,
-      country: "USA",
-      currency: "U.S. Dollar (USD)",
-      rate: 1.2359,
-    },
-    {
-      id: 3,
-      country: "Australia",
-      currency: "Australian Dollar (AUD)",
-      rate: 1.8501,
-    },
-  ];
-
-  res.status(200).json(data);
-}
+export async function GET() {
+    return NextResponse.json([
+        {
+            id: 1,
+            country: "European Union",
+            currency: "Euro (EUR)",
+            rate: 1.1348
+        },
+        {
+            id: 2,
+            country: "USA",
+            currency: "U.S. Dollar (USD)",
+            rate: 1.2359
+        },
+        {
+            id: 3,
+            country: "Australia",
+            currency: "Australian Dollar (AUD)",
+            rate: 1.8501
+        }
+    ]);
+};
