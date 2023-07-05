e9279c1dea44dfec85416e44559ec872bbc7d625
James Merriman <mezzarino@outlook.com>
2023-07-05 16:16:35 +0100

    Tweak headers across all projects

--- apps/apis/next.config.js
+++ apps/apis/next.config.js
@@ -1,8 +1,8 @@
 const securityHeaders = [
-
   { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
   { key: 'X-Frame-Options', value: 'DENY' },
   { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'Access-Control-Allow-Origin', value: 'https://poc-multisite-apis.vercel.app' },
   { key: 'X-DNS-Prefetch-Control', value: 'on' },
   { key: 'X-XSS-Protection', value: '1; mode=block' },
   { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
--- apps/payment-funnel/src/middleware.ts
+++ apps/payment-funnel/src/middleware.ts
@@ -1,40 +1,43 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from 'next/server';
 
 export function generateCsp() {
   const nonce = crypto.randomUUID();
 
+  const devScriptPolicy = "'unsafe-eval'";
+  const isProduction = process.env.NODE_ENV === 'production';
+
   const csp = [
-    { name: "default-src", values: ["'self'"] },
+    { name: 'default-src', values: ["'none'"] },
     {
-      name: "script-src",
+      name: 'script-src',
       values: [
         "'self'",
-        `'unsafe-eval'`,
         `'unsafe-inline'`,
-      ],
+      ].concat(isProduction ? [] : devScriptPolicy),
     },
     {
-      name: "style-src",
+      name: 'style-src',
       values: ["'self'", `'nonce-${nonce}'`],
     },
     {
-      name: "connect-src",
-      values: ["'self'", "*.vercel.app"],
+      name: 'connect-src',
+      values: ["'self'", '*.vercel.app'],
     },
-    { name: "font-src", values: ["'self'", "data:"] },
-    { name: "img-src", values: ["'self'", "data:"] },
-    { name: "worker-src", values: ["'self'", "blob:"] },
-    { name: "media-src", values: ["'none'"] },
-    { name: "object-src", values: ["'none'"] },
-    { name: "frame-ancestors", values: ["'none'"] },
-    { name: "form-action", values: ["'self'"] },
+    { name: 'font-src', values: ["'self'", 'data:'] },
+    { name: 'img-src', values: ["'self'", 'data:'] },
+    { name: 'worker-src', values: ["'self'", 'blob:'] },
+    { name: 'media-src', values: ["'none'"] },
+    { name: 'object-src', values: ["'none'"] },
+    { name: 'frame-ancestors', values: ["'none'"] },
+    { name: 'form-action', values: ["'self'"] },
+    { name: 'base-uri', values: ["'none'"] },
   ];
 
   const cspString = csp
     .map((directive) => {
-      return `${directive.name} ${directive.values.join(" ")}`;
+      return `${directive.name} ${directive.values.join(' ')}`;
     })
-    .join("; ");
+    .join('; ');
 
   return { csp: cspString, nonce };
 }
@@ -47,9 +50,9 @@ export async function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
 
   // set nonce request header to read in pages if needed
-  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set('x-nonce', nonce);
 
-  const headerKey = "content-security-policy";
+  const headerKey = 'content-security-policy';
 
   requestHeaders.set(headerKey, csp);
 
